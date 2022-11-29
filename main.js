263738200097cd37a65cb955746f463e1be64c26
midnightstarlight <malabaopie@gmail.com>
2022-11-29 23:50:45 +0800

    add model and simple rotation animation

--- main.js
+++ main.js
@@ -4,6 +4,7 @@
 // import {loadGLTF, loadAudio, loadVideo} from "./libs/loader.js";
 // import {CSS3DObject} from "./libs/three.js-r132/examples/jsm/renderers/CSS3DRenderer.js";
 
+import {loadGLTF} from "../../libs/loader.js";
 
 const THREE = window.MINDAR.IMAGE.THREE;
 
@@ -17,8 +18,11 @@ document.addEventListener('DOMContentLoaded', () => {
     });
     const {renderer, scene, camera} = mindarThree;
 
+    const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
+    scene.add(light);
+
     const geometry = new THREE.PlaneGeometry(1, 1);
-    const material = new THREE.MeshBasicMaterial({color: 0x00ffff, transparent: true, opacity: 0.5});
+    const material = new THREE.MeshBasicMaterial({color: 0xffffff, transparent: true, opacity: 0.8});
     // const plane = new THREE.Mesh(geometry, material);
 
     const anchor = [];
@@ -32,16 +36,25 @@ document.addEventListener('DOMContentLoaded', () => {
         console.log("loop " + i);
     }
 
-    // const anchor_1 = mindarThree.addAnchor(0);
-    // anchor_1.group.add(plane);
-    //
-    // const anchor_2 = mindarThree.addAnchor(1);
-    // anchor_2.group.add(plane);
+    const gltf = await loadGLTF('../../assets/models/letters/letter_p.gltf');
+    gltf.scene.scale.set(1,1,1);
+    gltf.scene.position.set(0, 0, 0.5);
+
+    // anchor[0] = mindarThree.addAnchor(0);
+    anchor[0].group.add(gltf.scene);
+
+    const mixer = new THREE.AnimationMixer(gltf.scene);
+
+
+    const clock = new THREE.Clock();
 
 
     await mindarThree.start();
     renderer.setAnimationLoop(() => {
-      renderer.render(scene, camera);
+        const delta = clock.getDelta();
+        gltf.scene.rotation.set(0, gltf.scene.rotation.y+delta, 0);
+        mixer.update(delta);
+        renderer.render(scene, camera);
     });
   }
   start();
