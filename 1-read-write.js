795f5d814620e71055f04be1e89f1a70ef34fc5d
midnightstarlight <malabaopie@gmail.com>
2023-02-08 00:55:00 +0800

    test write absolute url

--- 1-read-write.js
+++ 1-read-write.js
@@ -4,6 +4,7 @@ var nfc = {
   hWrite : null, // html write button
   hRead : null, // html read button
   hMsg : null, // html "console messages"
+
   init : () => {
     // (A1) GET HTML ELEMENTS
     nfc.hTxt = document.getElementById("demoT"),
@@ -37,13 +38,13 @@ var nfc = {
 
     ndef.write({
       records: [
-        // { recordType:"text", data:"Hello World" },
+        { recordType:"absolute-url", data:"https://www.instagram.com/thesingaporedis/?igshid=YmMyMTA2M2Y%3D" },
         // { recordType:"url", data:"https://code-boxx.com/" },
         // { recordType:"url", data:"sms:12345678" },
         // { recordType:"url", data:"tel:12345678" },
         // { recordType:"url", data:"mailto:jon@doe.com?subject=Title&body=Body" },
         // { recordType:"url", data:"geo:35.698723,139.772639" },
-         { recordType:"empty" }
+         // { recordType:"empty" }
       ]
     })
     .then(() => nfc.logger("Write OK"))
