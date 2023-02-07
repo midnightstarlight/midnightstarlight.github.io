a08de11cca4df7986d47de5cb3f9213046fbb4c2
midnightstarlight <malabaopie@gmail.com>
2023-02-08 00:38:12 +0800

    update to write empty tag

--- 1-read-write.js
+++ 1-read-write.js
@@ -37,13 +37,13 @@ var nfc = {
 
     ndef.write({
       records: [
-        { recordType:"text", data:"Hello World" },
-        { recordType:"url", data:"https://code-boxx.com/" },
-        { recordType:"url", data:"sms:12345678" },
-        { recordType:"url", data:"tel:12345678" },
-        { recordType:"url", data:"mailto:jon@doe.com?subject=Title&body=Body" },
-        { recordType:"url", data:"geo:35.698723,139.772639" },
-        // { recordType:"empty" }
+        // { recordType:"text", data:"Hello World" },
+        // { recordType:"url", data:"https://code-boxx.com/" },
+        // { recordType:"url", data:"sms:12345678" },
+        // { recordType:"url", data:"tel:12345678" },
+        // { recordType:"url", data:"mailto:jon@doe.com?subject=Title&body=Body" },
+        // { recordType:"url", data:"geo:35.698723,139.772639" },
+         { recordType:"empty" }
       ]
     })
     .then(() => nfc.logger("Write OK"))
