5212802bf10123292962562e069289c90a5f53a7
midnightstarlight <malabaopie@gmail.com>
2023-02-08 00:28:36 +0800

    testing write a bunch of records to nfc tag

--- 1-read-write.js
+++ 1-read-write.js
@@ -31,10 +31,28 @@ var nfc = {
   write : () => {
     nfc.logger("Approach NFC Tag");
     const ndef = new NDEFReader();
-    ndef.write(nfc.hTxt.value)
+    // ndef.write(nfc.hTxt.value)
+    // .then(() => nfc.logger("Write OK"))
+    // .catch(err => nfc.logger("ERROR - " + err.message));
+
+    ndef.write({
+      records: [
+        { recordType:"text", data:"Hello World" },
+        { recordType:"url", data:"https://code-boxx.com/" },
+        { recordType:"url", data:"sms:12345678" },
+        { recordType:"url", data:"tel:12345678" },
+        { recordType:"url", data:"mailto:jon@doe.com?subject=Title&body=Body" },
+        { recordType:"url", data:"geo:35.698723,139.772639" },
+        { recordType:"empty" }
+      ]
+    })
     .then(() => nfc.logger("Write OK"))
     .catch(err => nfc.logger("ERROR - " + err.message));
   },
+
+
+
+
 
   // (D) READ NFC TAG
   read : () => {
